77c085c1efe3c9518c353dfaae3d3972dcc3d124
song <dubbsong@gmail.com>
2020-03-29 10:55:12 +0900

    Set TV Shows data with Axios

--- pages/tv.vue
+++ pages/tv.vue
@@ -1,9 +1,130 @@
 <template>
   <div id="tv">
+    <!-- Popular -->
     <section class="section">
       <div class="container">
-        <p class="title">TV Shows</p>
+        <!-- title -->
+        <p class="subtitle">Popular</p>
+
+        <!-- collapse btn -->
+        <b-collapse :open="false">
+          <button slot="trigger" class="button is-light">
+            Data
+          </button>
+
+          <div class="content">
+            <p>{{ popularData }}</p>
+          </div>
+        </b-collapse>
+      </div>
+    </section>
+
+    <!-- On The Air -->
+    <section class="section">
+      <div class="container">
+        <!-- title -->
+        <p class="subtitle">On The Air</p>
+
+        <!-- collapse btn -->
+        <b-collapse :open="false">
+          <button slot="trigger" class="button is-light">
+            Data
+          </button>
+
+          <div class="content">
+            <p>{{ onTheAirData }}</p>
+          </div>
+        </b-collapse>
+      </div>
+    </section>
+
+    <!-- Top Rated -->
+    <section class="section">
+      <div class="container">
+        <!-- title -->
+        <p class="subtitle">Top Rated</p>
+
+        <!-- collapse btn -->
+        <b-collapse :open="false">
+          <button slot="trigger" class="button is-light">
+            Data
+          </button>
+
+          <div class="content">
+            <p>{{ topRatedData }}</p>
+          </div>
+        </b-collapse>
       </div>
     </section>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data: () => ({
+    popularData: [],
+    onTheAirData: [],
+    topRatedData: []
+  }),
+  mounted() {
+    this.getPopularData()
+    this.getOnTheAirData()
+    this.getTopRatedData()
+  },
+  methods: {
+    // Popular Data
+    getPopularData() {
+      const params = [
+        'api_key=b1aff257ceb0cbcdd236cef217694a61',
+        'language=en-US'
+      ].join('&')
+
+      axios
+        .get(`https://api.themoviedb.org/3/tv/popular?${params}`)
+        .then(({ data }) => {
+          this.popularData = data.results
+        })
+        .catch((error) => {
+          this.popularData = []
+          throw error
+        })
+    },
+    // On The Air Data
+    getOnTheAirData() {
+      const params = [
+        'api_key=b1aff257ceb0cbcdd236cef217694a61',
+        'language=en-US'
+      ].join('&')
+
+      axios
+        .get(`https://api.themoviedb.org/3/tv/on_the_air?${params}`)
+        .then(({ data }) => {
+          this.onTheAirData = data.results
+        })
+        .catch((error) => {
+          this.onTheAirData = []
+          throw error
+        })
+    },
+    // Top Rated Data
+    getTopRatedData() {
+      const params = [
+        'api_key=b1aff257ceb0cbcdd236cef217694a61',
+        'language=en-US'
+      ].join('&')
+
+      axios
+        .get(`https://api.themoviedb.org/3/tv/top_rated?${params}`)
+        .then(({ data }) => {
+          this.topRatedData = data.results
+        })
+        .catch((error) => {
+          this.topRatedData = []
+          throw error
+        })
+    }
+  }
+}
+</script>
