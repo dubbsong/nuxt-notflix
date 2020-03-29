81ed8725e5d6a4362da0bd4b2360e3835a27c600
song <dubbsong@gmail.com>
2020-03-29 10:45:50 +0900

    Set movies data with Axios

--- pages/movies.vue
+++ pages/movies.vue
@@ -3,24 +3,57 @@
     <!-- Popular -->
     <section class="section">
       <div class="container">
+        <!-- title -->
         <p class="subtitle">Popular</p>
-        <p>{{ popularData }}</p>
+
+        <!-- collapse btn -->
+        <b-collapse :open="false">
+          <button slot="trigger" class="button is-light">
+            Data
+          </button>
+
+          <div class="content">
+            <p>{{ popularData }}</p>
+          </div>
+        </b-collapse>
       </div>
     </section>
 
     <!-- Now Playing -->
     <section class="section">
       <div class="container">
+        <!-- title -->
         <p class="subtitle">Now Playing</p>
-        <p>{{ nowPlayingData }}</p>
+
+        <!-- collapse btn -->
+        <b-collapse :open="false">
+          <button slot="trigger" class="button is-light">
+            Data
+          </button>
+
+          <div class="content">
+            <p>{{ nowPlayingData }}</p>
+          </div>
+        </b-collapse>
       </div>
     </section>
 
     <!-- Top Rated -->
     <section class="section">
       <div class="container">
+        <!-- title -->
         <p class="subtitle">Top Rated</p>
-        <p>{{ topRatedData }}</p>
+
+        <!-- collapse btn -->
+        <b-collapse :open="false">
+          <button slot="trigger" class="button is-light">
+            Data
+          </button>
+
+          <div class="content">
+            <p>{{ topRatedData }}</p>
+          </div>
+        </b-collapse>
       </div>
     </section>
   </div>
