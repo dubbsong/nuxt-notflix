473e4e2b6950cff5de6524a16041d1e086ecdfb6
song <dubbsong@gmail.com>
2020-03-29 11:01:23 +0900

    Set Home but not yet

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,40 @@
 <template>
   <div id="home">
+    <!-- My List -->
     <section class="section">
       <div class="container">
-        <p class="title">Home</p>
+        <!-- title -->
+        <p class="subtitle">My List</p>
+
+        <!-- collapse btn -->
+        <b-collapse :open="false">
+          <button slot="trigger" class="button is-light">
+            Data
+          </button>
+
+          <div class="content">
+            <p>{{}}</p>
+          </div>
+        </b-collapse>
+      </div>
+    </section>
+
+    <!-- Trending Now -->
+    <section class="section">
+      <div class="container">
+        <!-- title -->
+        <p class="subtitle">Trending Now</p>
+
+        <!-- collapse btn -->
+        <b-collapse :open="false">
+          <button slot="trigger" class="button is-light">
+            Data
+          </button>
+
+          <div class="content">
+            <p>{{}}</p>
+          </div>
+        </b-collapse>
       </div>
     </section>
   </div>
