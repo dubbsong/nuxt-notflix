dc1f3238dd630dcc90bf2e91a876174ef32aed2f
song <dubbsong@gmail.com>
2020-03-31 07:42:30 +0900

    Set Login but not yet

--- pages/login.vue
+++ pages/login.vue
@@ -2,8 +2,61 @@
   <div id="login">
     <section class="section">
       <div class="container">
-        <p class="title">Login</p>
+        <div class="card">
+          <div class="card-content">
+            <!-- Title -->
+            <p class="title">LOGIN</p>
+
+            <!-- ID -->
+            <b-field label="ID" :label-position="labelPosition">
+              <b-input
+                v-model="userId"
+                type="text"
+                placeholder="Please enter your ID"
+              ></b-input>
+            </b-field>
+
+            <!-- Password -->
+            <b-field label="Password" :label-position="labelPosition">
+              <b-input
+                v-model="userPassword"
+                type="password"
+                placeholder="Please enter your Password"
+              ></b-input>
+            </b-field>
+
+            <!-- Btn -->
+            <b-button type="is-light" expanded>LOGIN</b-button>
+          </div>
+        </div>
       </div>
     </section>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    userId: '',
+    userPassword: '',
+    labelPosition: 'on-border'
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+#login {
+  section {
+    .container {
+      .card {
+        // background-color: rgba(0, 0, 0, 0.75);
+        background-color: #20a19c;
+        width: 440px;
+        margin: 0 auto;
+        border-radius: 4px;
+        box-shadow: 4px 2px 16px rgba(0, 0, 0, 0.04);
+      }
+    }
+  }
+}
+</style>
